9a0deed24383cbfcdb938a37499d71fc3149da05
Kim Thompson <kimthompson@hey.com>
2021-02-26 12:52:26 -0600

    rna solution

--- javascript/rnaTranscription/rna-transcription.js
+++ javascript/rnaTranscription/rna-transcription.js
@@ -1,8 +1,16 @@
-//
 // This is only a SKELETON file for the 'RNA Transcription' exercise. It's been provided as a
 // convenience to get you started writing code faster.
-//
 
-export const toRna = () => {
-  throw new Error("Remove this statement and implement this function");
+export const toRna = (dna) => {
+  let rna = dna.split('');
+
+  const rnaReducer = (acc, element) => {
+    if (element === "C") return `${acc}G`
+    if (element === "G") return `${acc}C`
+    if (element === "T") return `${acc}A`
+    if (element === "A") return `${acc}U`
+    return `${acc}${element}`
+  }
+
+  return rna.reduce(rnaReducer, '')
 };
